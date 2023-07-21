6dd496835000df2b79c90558b35588453c87759e
piyush-gupta123 <95485177+piyush-gupta123@users.noreply.github.com>
2023-07-22 00:00:01 +0530

    Google Authentication Completed

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,49 +1,46 @@
 import NextAuth from "next-auth/next";
-import GoogleProvider from 'next-auth/providers/google';
+import GoogleProvider from "next-auth/providers/google";
 import { connectedToDB } from "@utils/database";
-import User from '@models/user';
+import User from "@models/user";
 
 const handler = NextAuth({
-    providers: [
-        GoogleProvider({
-            client: '',
-            clientSecret: ''
-        })
-    ],
-    async session({ session }){
-        const sessionUser = User.findOne({
-            email: session.user.email
-        })
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  async session({ session }) {
+    const sessionUser = User.findOne({
+      email: session.user.email,
+    });
 
-        session.user.id = sessionUser._id.toString();
+    session.user.id = sessionUser._id.toString();
 
-        return session
-    },
-    async signIn({ profile }){
-        try{
-            await connectedToDB();
+    return session;
+  },
+  async signIn({ profile }) {
+    try {
+      await connectedToDB();
 
-            //find if user exists
-            const user = await User.findOne({email : profile.email})
+      //find if user exists
+      const user = await User.findOne({ email: profile.email });
 
-            //if not create new user
-            if(!user){
-                await User.create({
-                    email: profile.email,
-                    username: profile.name.replace(" ","").toLowerCase(),
-                    image: profile.picture
-                })
-            }
+      //if not create new user
+      if (!user) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
 
-
-
-            return true;
-        }
-        catch(err){
-            console.log(err);
-            return false;
-        }
+      return true;
+    } catch (err) {
+      console.log(err);
+      return false;
     }
-})
+  },
+});
 
 export { handler as GET, handler as POST };
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -21,7 +21,7 @@ const Nav = () => {
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
         <Image
-          src={session?.user.image}
+          src="assets/images/logo.svg"
           alt="Prompton Logo"
           width={30}
           height={30}
@@ -41,7 +41,7 @@ const Nav = () => {
             </button>
             <Link href="/profile">
               <Image
-                src="assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
